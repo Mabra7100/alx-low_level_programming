eb19a845cb277fdf744123337e56565146bc800a
Sixolise <sixolisemabutklaas@gmail.com>
2023-09-10 14:15:26 -0700

    auto push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Return: return 0 when its working
+ *
+ */
+
+int main(void)
+{
+  int n;
+
+  for (n = 97; n <= 122; n++)
+    putchar(n);
+  putchar(10);
+  return(0);
+}

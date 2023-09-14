5c905c64f4c0af289e498bcc2a4127c58fc0c690
Sixolise <sixolisemabutklaas@gmail.com>
2023-09-14 06:50:20 -0700

    auto push

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,19 @@
  */
 int main(void)
 {
-printf("Infinite loop incoming :(\n")
+/*
+ * int i;
+ */
+printf("Infinite loop incoming :(\n");
+/*
+ * i = 0;
+ */
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ *  */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);

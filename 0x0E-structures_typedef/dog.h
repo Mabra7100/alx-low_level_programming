f2057d1a2e5ca1d5b46393f53b6662a91de49a4e
Sixolise <sixolisemabutklaas@gmail.com>
2023-10-09 04:21:42 -0700

    auto push

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H_INCLUDED
+#define DOG_H_INCLUDED
+
+/**
+ * struct dog - Stores all the elements bellow
+ * @name: The name of the dog
+ * @age:  The Age of the dog
+ * @owner: The name of the dog owner
+ *
+ * Description: this stores all the variables pass to the elements
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
+#endif

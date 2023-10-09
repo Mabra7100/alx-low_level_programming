44c799c698a8ab83f60ebb6ff98077da5da570b8
Sixolise <sixolisemabutklaas@gmail.com>
2023-10-09 05:06:25 -0700

    auto push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,8 @@
  * Description: this stores all the variables pass to the elements
  */
 
+typedef struct dog dog_t;
+
 struct dog
 {
 char *name;
@@ -20,6 +22,9 @@ float age;
 char *owner;
 };
 
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif

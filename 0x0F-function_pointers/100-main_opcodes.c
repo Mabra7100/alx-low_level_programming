0bdcd9bc9768ab32a14ed667dbba4645cc728bbc
Sixolise <sixolisemabutklaas@gmail.com>
2023-10-11 09:34:35 -0700

    auto push

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 
 /**
- * print_opcodes - Prints the opcodes of the function
- * @num_bytes: Number of bytes to print
- */
-void print_opcodes(int num_bytes)
-{
-	unsigned char *ptr = (unsigned char *)print_opcodes;
-	int i;
+*main -  program that prints the opcodes of its own main function.
+*@argc: number of arguments passed to the program
+*@argv: array of arguments
+*
+*Return: on success, 1 or 2 in case of failure
+*/
 
-	for (i = 0; i < num_bytes; ++i)
-	{
-		printf("%02x", *(ptr + i));
-	}
-	printf("\n");
-}
-
-/**
- * main - Entry point of the program
- * @argc: Number of command line arguments
- * @argv: Array of command line arguments
- * Return: 0 on success, 1 for incorrect arguments, 2 for negative bytes
- */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
 
-	int num_bytes = atoi(argv[1]);
+int index, bytes;
+int (*address)(int, char **) = main;
+unsigned char opcode;
 
-	if (num_bytes < 0)
-	{
-		printf("Error\n");
-		return (2);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
 
-	print_opcodes(num_bytes);
+bytes = atoi(argv[1]);
 
-	return (0);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (index = 0; index <  bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
+return (0);
 }

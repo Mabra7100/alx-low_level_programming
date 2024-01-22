c0e0ab730de4c45ec72d15a494cf7dff94328d33
Sixolise Klaas <141272926+Mabra7100@users.noreply.github.com>
2024-01-22 17:06:28 +0200

    2-main added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+
+
+/**
+*
+*main - function  prints the name of the file it was compiled from, followed by a new line.
+*
+*Return: returns 0
+*/
+
+
+int main() {
+printf("%s\n", __FILE__);
+return 0;
+}

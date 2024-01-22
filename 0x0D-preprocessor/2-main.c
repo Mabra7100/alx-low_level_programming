6c848086f03f903faa128596dc2ad3325b1e020d
Sixolise Klaas <141272926+Mabra7100@users.noreply.github.com>
2024-01-22 17:12:17 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,11 +4,9 @@
 
 
 /**
-*
-*main - function  prints the name of the file it was compiled from, followed by a new line.
-*
-*Return: returns 0
-*/
+ * main - prints the name of the file it was compiled from, followed by a new line.
+ * Return: returns 0
+ */
 
 
 int main() {

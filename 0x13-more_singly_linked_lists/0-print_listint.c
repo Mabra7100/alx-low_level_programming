5fa0ade789795a2a7133890388f0bf7a37aa90c5
sixolise <sixolisemabutiklaas@gmail.com>
2024-02-06 04:27:24 -0800

    Changes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t count = 0;
+	size_t nnodes = 0;
 
-while (h != NULL)
-{
-count++;
-printf("%d\n", h->n);
-h = h->next;
-}
-printf("\n");
-return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
